32db4467f8387e6ddfda4bba127f77cfcf8352f2
Hardeep Singh <ootz0rz@gmail.com>
2013-05-31 17:19:19 -0400

    initial commit + initial look/feel

---
+++ js/index.js
@@ -0,0 +1,17 @@
+/** CSS styles for each sphere color available */
+var sphere_colors = {
+    '0' : 'sphere1',
+    '1' : 'sphere2',
+    '2' : 'sphere3',
+    '3' : 'sphere4',
+    '4' : 'sphere5'
+};
+
+/** CSS styles for each square color available */
+var square_colors = {
+    '0' : 'square1',
+    '1' : 'square2',
+    '2' : 'square3',
+    '3' : 'square4',
+    '4' : 'square5'
+};
